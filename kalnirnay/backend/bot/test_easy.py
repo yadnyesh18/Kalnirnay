ee5e09d8f4b87282ff6cc84cc374bac37090fc44
Yadnyesh <studybuddy2techbuddy@gmail.com>
2026-03-21 22:44:00 +0530

    Second commit

---
+++ kalnirnay/backend/bot/test_easy.py
@@ -0,0 +1,9 @@
+import easyocr
+
+reader = easyocr.Reader(['en'])
+result = reader.readtext('temp_images/Hackniche_test2.jpeg', detail=0)
+
+print("\n--- EASYOCR TEXT ---")
+for line in result:
+    print(line)
+print("--------------------")
